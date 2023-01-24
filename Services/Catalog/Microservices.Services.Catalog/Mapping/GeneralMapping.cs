3230c556f2fc2c90938cfc1757e9b383ccfbf376
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-01-24 17:13:58 +0300

    Automapper Added

---
+++ Services/Catalog/Microservices.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microservices.Services.Catalog.Dtos;
+using Microservices.Services.Catalog.Models;
+
+namespace Microservices.Services.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+           
+            CreateMap<CourseCreateDto, Course>().ReverseMap();
+            CreateMap<CourseUpdateDto, Course>().ReverseMap();
+        }
+    }
+}
