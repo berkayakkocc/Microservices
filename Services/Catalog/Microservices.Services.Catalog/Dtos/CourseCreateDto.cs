d3fac8467063af4535bc72790c6f8b5b90338b48
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-01-24 17:07:47 +0300

    Dtos Added For Catalog

---
+++ Services/Catalog/Microservices.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using Microservices.Services.Catalog.Models;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Microservices.Services.Catalog.Dtos
+{
+    public class CourseCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+        public string CategoryId { get; set; }
+
+        public FeatureDto Feature { get; set; }
+
+
+    }
+}
---
+++ Services/Catalog/Microservices.Services.Catalog/Dtos/CourseUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Microservices.Services.Catalog.Dtos
+{
+    public class CourseUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+        public string CategoryId { get; set; }
+
+        public FeatureDto Feature { get; set; }
+    }
+}
