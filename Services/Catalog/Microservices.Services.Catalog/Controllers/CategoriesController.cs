aee726b15d03ae95954d848b6c310ae8b96ccd90
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-01-26 17:00:56 +0300

    Categories Controller

---
+++ Services/Catalog/Microservices.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Microservices.Services.Catalog.Dtos;
+using Microservices.Services.Catalog.Services;
+using Microservices.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService ;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            var response = await _categoryService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+       
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryDto);
+
+            return CreateActionResultInstance(response);
+        }
+      
+    }
+}
